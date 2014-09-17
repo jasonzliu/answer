847e180f78ecd22f957ea2746b5be1c699b25654
jasonzliu <jasonzliu@yahoo.ca>
2014-09-17 16:58:32 +0000

    updated kpi

--- public/js/kpiCtrl.js
+++ public/js/kpiCtrl.js
@@ -3,15 +3,25 @@ angular
     .controller('kpiCtrl', function ($scope, $http, $q) {
 
         var gauges = [];
-
-        function createGauge(name, label, min, max)
+        $scope.campaignType = "";
+        $scope.mediaType = "";
+        $scope.region = "";
+        $scope.product = "";
+        $scope.program = "";
+        $scope.type = "Complete";
+        
+        function createGauge(name, label, value)
         {
+            var ran1 = Math.floor((Math.random() * 30) + 15);
+            var ran2 = Math.floor((Math.random() * 30) + 15);
+            var min = Math.round ((value - value * ran1 /100 ) /100) * 100;
+            var max = Math.round ((value + value * ran2 /100 ) /100) * 100;
             var config =
             {
                 size: 200,
                 label: label,
-                min: undefined != min ? min : 0,
-                max: undefined != max ? max : 100,
+                min: min ,
+                max: max,
                 minorTicks: 5
             };
 
@@ -21,14 +31,6 @@ angular
 
             gauges[name] = new Gauge(name + "GaugeContainer", config);
             gauges[name].render();
-        }
-
-        function createGauges(name, text)
-        {
-            createGauge(name + "_per_sale", text + " Per Sale", 2000, 3000);
-            createGauge(name + "_per_call", text + " Per Call", 200, 300);
-            createGauge(name + "_per_click", text + " Per Click", 0, 100);
-            //createGauge("test", "Test", -50, 50 );
         }
 
         function updateGauge(name, value)
@@ -46,27 +48,39 @@ angular
         $scope.dtFrom =new Date(new Date().setDate($scope.dtTo.getDate()-30));
 
         $scope.query = function(){
+            /*console.log($scope.campaignType);
+            console.log($scope.program);
+            console.log($scope.mediaType);
+            console.log($scope.region);
+            console.log($scope.product);*/
             d3.selectAll("svg").remove();
             var arr1 = [$http.get('kpi/cost_per_sale'), $http.get('kpi/cost_per_call'), $http.get('kpi/cost_per_click')];
             $q.all(arr1)
                 .then(function(result){
-                    createGauge("cost_per_sale", "Cost Per Sale", 2000, 3000);
-                    createGauge("cost_per_call", "Cost Per Call", 200, 300);
-                    createGauge("cost_per_click", "Cost Per Click", 0, 100);
-                    updateGauge("cost_per_sale", (Number(result[0].data[0].value.replace(/,/g, ''))));
-                    updateGauge("cost_per_call", (Number(result[1].data[0].value.replace(/,/g, ''))));
-                    updateGauge("cost_per_click", (Number(result[2].data[0].value.replace(/,/g, ''))));
+                    var cost_per_sale = Number(result[0].data[0].value.replace(/,/g, ''));
+                    var cost_per_call = Number(result[1].data[0].value.replace(/,/g, ''));
+                    var cost_per_click = Number(result[2].data[0].value.replace(/,/g, ''));
+                    createGauge("cost_per_sale", "Cost Per Sale", cost_per_sale);
+                    createGauge("cost_per_call", "Cost Per Call", cost_per_call);
+                    createGauge("cost_per_click", "Cost Per Click", cost_per_click);
+                    updateGauge("cost_per_sale", cost_per_sale);
+                    updateGauge("cost_per_call", cost_per_call);
+                    updateGauge("cost_per_click", cost_per_click);
                 });
 
             var arr2 = [$http.get('kpi/quantity_per_sale'), $http.get('kpi/quantity_per_call'), $http.get('kpi/quantity_per_click')];
             $q.all(arr2)
                 .then(function(result){
-                    createGauge("quantity_per_sale", "Quantity Per Sale", 5000, 10000);
-                    createGauge("quantity_per_call", "Quantity Per Call", 1000, 1500);
-                    createGauge("quantity_per_click", "Quantity Per Click", 10000, 15000);
-                    updateGauge("quantity_per_sale", (Number(result[0].data[0].value.replace(/,/g, ''))));
-                    updateGauge("quantity_per_call", (Number(result[1].data[0].value.replace(/,/g, ''))));
-                    updateGauge("quantity_per_click", (Number(result[2].data[0].value.replace(/,/g, ''))));
+                    var quantity_per_sale = Number(result[0].data[0].value.replace(/,/g, ''));
+                    var quantity_per_call = Number(result[1].data[0].value.replace(/,/g, ''));
+                    var quantity_per_click = Number(result[2].data[0].value.replace(/,/g, ''));
+                    
+                    createGauge("quantity_per_sale", "Quantity Per Sale", quantity_per_sale);
+                    createGauge("quantity_per_call", "Quantity Per Call", quantity_per_call);
+                    createGauge("quantity_per_click", "Quantity Per Click", quantity_per_click);
+                    updateGauge("quantity_per_sale", quantity_per_sale);
+                    updateGauge("quantity_per_call", quantity_per_call);
+                    updateGauge("quantity_per_click", quantity_per_click);
                 });
 
         };
