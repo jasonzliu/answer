bd0eec566b642417786982d9383473acc6cbf23d
jason liu <jliu@terremark.com>
2014-09-17 14:47:33 -0500

    file upload

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,1 @@
-angular.module('the-answer', ['ui.router', 'ngProgress', 'textAngular']);
+angular.module('the-answer', ['ui.router', 'ngProgress', 'textAngular', 'angularFileUpload']);
--- server.js
+++ server.js
@@ -2,8 +2,10 @@ var http = require('http');
 var express = require('express');
 var sqlApi = require('./server/sqlapi.js');
 var path = require('path');
+var multipart = require('connect-multiparty');
+var _ = require('lodash');
 
-console.log('Server running at http://127.0.0.1:7777/');
+console.log('Server running at http://'+ process.env.IP + ':'+process.env.PORT);
 
 /*http.createServer(function (req, res) {
     var arr = _.compact(req.url.split('/'));
@@ -26,11 +28,26 @@ console.log('Server running at http://127.0.0.1:7777/');
 var api = new sqlApi();
 var app = express();
 
+app.use(multipart({
+    uploadDir: 'upload'
+}));
 console.log("\n-------------the answer-------------\n");
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.bodyParser());
 // register api routes
 app.get('*', api.runsql);
+
+app.post('/upload', function(req, res){
+    var data = _.pick(req.body, 'type')
+        , uploadPath = path.join(process.cwd(), 'uploads')
+        , file = req.files.file;
+
+    console.log(file.name); //original name (ie: sunset.png)
+    console.log(file.path); //tmp path (ie: /tmp/12345-xyaz.png)
+    console.log(uploadPath); //uploads directory: (ie: /home/user/data/uploads)
+    res.end();
+});
 
 http.createServer(app).listen(process.env.PORT, '172.17.113.157');
 
