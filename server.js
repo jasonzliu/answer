7ed1d083e87547eb41af1d53587572f75cbdd2a2
jason liu <jliu@terremark.com>
2014-09-18 16:13:10 -0500

    removed db part

--- server.js
+++ server.js
@@ -32,5 +32,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 // register api routes
 app.get('*', api.runsql);
 
-http.createServer(app).listen(7777, '127.0.0.1');
+//http.createServer(app).listen(7777, '127.0.0.1');
+http.createServer(app).listen(process.env.PORT, '172.17.113.157');
 
