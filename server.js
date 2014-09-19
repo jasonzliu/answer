5818b4b91d3c7fdcfe93f22fff6a3b2e5528a49e
jason liu <jliu@terremark.com>
2014-09-19 10:58:37 -0500

    removed db

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 var http = require('http');
 var express = require('express');
-var sqlApi = require('./server/sqlapi.js');
 var path = require('path');
 
 console.log('Server running at http://127.0.0.1:7777/');
-var api = new sqlApi();
 var app = express();
 
 console.log("\n-------------the answer-------------\n");
 
 app.use(express.static(path.join(__dirname, 'public')));
 // register api routes
-app.get('*', api.runsql);
+app.get('*', function(req, res){res.end();});
 
 //http.createServer(app).listen(7777, '127.0.0.1');
 http.createServer(app).listen(process.env.PORT, process.env.IP);
