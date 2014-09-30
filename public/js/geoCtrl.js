82c14d3b4059dcf56640634e49b805399acb3643
jason liu <jliu@terremark.com>
2014-09-30 16:50:13 -0500

    geo

--- public/js/geoCtrl.js
+++ public/js/geoCtrl.js
@@ -14,12 +14,29 @@ angular
             "MI", "WY", "MT", "ID", "WA", "DC", "TX", "CA", "AZ", "NV", "UT",
             "CO", "NM", "OR", "ND", "SD", "NE", "IA", "MS", "IN", "IL", "MN",
             "WI", "MO", "AR", "OK", "KS", "LS", "VA"]
-            .forEach(function(d){
+        .forEach(function(d){
                 //sampleData[d]={budget:Math.round(100*Math.random()), sales:Math.round(100*Math.random()), color:d3.interpolate("#ffffcc", "#800026")(Math.random())};
                 sampleData[d]={budget:Math.round(100*Math.random()), sales:Math.round(100*Math.random()), color:"hsl(" + Math.random() * 360 + ",70%,60%)"};
             });
 
         /* draw states on id #statesvg */
         uStates.draw("#statesvg", sampleData, tooltipHtml);
+
+        var arr = [];
+        _.forOwn(sampleData, function(item, key){
+            var obj = {
+                'name': key,
+                'budget': item.budget,
+                'sales': item.sales
+            };
+            arr.push(obj);
+        });
+        arr = _.sortBy(arr, 'name');
+        $scope.firstHalf = _.filter(arr, function(item){
+            return item.name <= 'MS'});
+        $scope.firstHalf.unshift({'name': 'STATE', 'budget': 'BUDGET(million $)', 'sales': 'SALES(million $)'});
+        $scope.secondtHalf = _.filter(arr, function(item){
+            return item.name > 'MS'});
+        $scope.secondtHalf.unshift({'name': 'STATE', 'budget': 'BUDGET(million $)', 'sales': 'SALES(million $)'});
     });
 
