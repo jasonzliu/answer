780a2de336033677b473b4d102985fe49ff5e1ea
jason liu <jliu@terremark.com>
2014-10-21 16:31:03 -0500

    added rio

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ http.createServer(app).listen(7777, '100.79.164.6');
 //rio.enableRecordMode(true, {fileName: 'dump.bin'});
 //rio.enablePlaybackMode(true, {fileName: 'dump.bin'});
 
-rio.evaluate("pi / 2 * 2", options);
-rio.evaluate("c(1, 2)", options);
-rio.evaluate("as.character('Hello World')", options);
-rio.evaluate("c('a', 'b')", options);
-rio.evaluate("Sys.sleep(5); 11", options);
+rio.evaluate("pi / 2 * 2");
+rio.evaluate("c(1, 2)");
+rio.evaluate("as.character('Hello World')");
+rio.evaluate("c('a', 'b')");
+rio.evaluate("Sys.sleep(5); 11");
