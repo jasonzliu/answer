53b6672d68e1d797498eb6f3e3234326e0245f3d
jason liu <jliu@terremark.com>
2014-10-21 16:27:02 -0500

    added rio

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var express = require('express');
 var path = require('path');
+var rio = require('rio');
 
 console.log('Server running at http://127.0.0.1:7777/');
 var app = express();
@@ -12,5 +13,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('*', function(req, res){res.end();});
 
 //http.createServer(app).listen(7777, '127.0.0.1');
-http.createServer(app).listen(7777, '10.102.242.79');
+http.createServer(app).listen(7777, '100.79.164.6');
+//rio.enableDebug(true);
+//rio.enableRecordMode(true, {fileName: 'dump.bin'});
+//rio.enablePlaybackMode(true, {fileName: 'dump.bin'});
 
+rio.evaluate("pi / 2 * 2", options);
+rio.evaluate("c(1, 2)", options);
+rio.evaluate("as.character('Hello World')", options);
+rio.evaluate("c('a', 'b')", options);
+rio.evaluate("Sys.sleep(5); 11", options);
