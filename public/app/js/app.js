72c5b8703e41645ab341ace5a9f0e6e9ffdd04e5
jason liu <jliu@terremark.com>
2014-10-23 11:15:42 -0500

    added weekly spending to data summary

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -1732,12 +1732,12 @@ App.controller('mmoController', function($scope, $timeout){
     reset();
 
 var p = $timeout(function(){
-    function svgSales(){
-        var margin = {top: 30, right: 20, bottom: 30, left: 40},
-            width = 960 - margin.left - margin.right,
-            height = 400 - margin.top - margin.bottom;
+    var margin = {top: 30, right: 20, bottom: 30, left: 100},
+        width = 960 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
+    var parseDate = d3.time.format("%m/%d/%Y").parse;
 
-        var parseDate = d3.time.format("%m/%d/%Y").parse;
+    function svgSales(){
 
         var x = d3.time.scale()
             .range([0, width]);
@@ -1764,7 +1764,7 @@ var p = $timeout(function(){
             .append("g")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-        d3.csv("data/sale.csv", function(error, data) {
+        d3.csv("data/total_sale.csv", function(error, data) {
             data.forEach(function(d) {
                 d.date = parseDate(d.date);
                 d.sale = d.sale/1000000;
@@ -1804,6 +1804,98 @@ var p = $timeout(function(){
         });
     }
     svgSales();
+
+    function svgSpending(){
+
+        var x = d3.time.scale()
+            .range([0, width]);
+
+        var y = d3.scale.linear()
+            .range([height, 0]);
+
+        var color = d3.scale.category10();
+
+        var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient("bottom")
+            .tickFormat(d3.time.format("%m/%d/%Y"));
+
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient("left");
+
+        var line = d3.svg.line()
+            .interpolate("basis")
+            .x(function(d) { return x(d.date); })
+            .y(function(d) { return y(d.temperature); });
+
+        var svg = d3.select("#weeklySpending").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        d3.csv("data/weekly_spending.csv", function(error, data) {
+            color.domain(d3.keys(data[0]).filter(function(key) { return key !== "date" && key !== "week"; }));
+
+            data.forEach(function(d) {
+                d.date = parseDate(d.date);
+            });
+
+            var cities = color.domain().map(function(name) {
+                return {
+                    name: name,
+                    values: data.map(function(d) {
+                        return {date: d.date, temperature: d[name]};
+                    })
+                };
+            });
+
+            x.domain(d3.extent(data, function(d) { return d.date; }));
+
+            y.domain([
+                d3.min(cities, function(c) { return d3.min(c.values, function(v) { return v.temperature; }); }),
+                d3.max(["200000"])
+            ]);
+
+            svg.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + height + ")")
+                .call(xAxis);
+
+            svg.append("g")
+                .attr("class", "y axis")
+                .call(yAxis)
+                .append("text")
+                .attr("transform", "rotate(-90)")
+                .attr("y", 6)
+                .attr("dy", ".71em")
+                .style("text-anchor", "end")
+                .text("每周开销(美元)")
+                .style("font-size", "12px");
+
+            var city = svg.selectAll(".city")
+                .data(cities)
+                .enter().append("g")
+                .attr("class", "city");
+
+            city.append("path")
+                .attr("class", "line")
+                .attr("d", function(d) { return line(d.values); })
+                .style("stroke", function(d) { return color(d.name); });
+
+            svg.append("text")
+                .attr("x", (width / 2))
+                .attr("y", 0 - (margin.top / 2))
+                .attr("text-anchor", "middle")
+                .style("font-size", "16px")
+                .style("text-decoration", "underline")
+                .text("每周开销");
+        });
+    }
+
+    svgSpending();
+
 }, 1000);
 
 
