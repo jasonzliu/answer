ca522389cd7fc1dbc2d3dcdddffc5bcb9acee2c5
jason liu <jliu@terremark.com>
2014-11-03 11:37:50 -0600

    R

--- server.js
+++ server.js
@@ -18,8 +18,18 @@ http.createServer(app).listen(7777, '100.79.164.6');
 //rio.enableRecordMode(true, {fileName: 'dump.bin'});
 //rio.enablePlaybackMode(true, {fileName: 'dump.bin'});
 
-rio.evaluate("pi / 2 * 2");
-rio.evaluate("c(1, 2)");
-rio.evaluate("as.character('Hello World')");
-rio.evaluate("c('a', 'b')");
-rio.evaluate("Sys.sleep(5); 11");
+//rio.evaluate("pi / 2 * 2");
+rio.sourceAndEval("R/opt_total.R", {
+    entryPoint: "getOptimal",
+    data: [500000],
+    callback: function(err, data){
+        if (!err) {
+            data = JSON.parse(data);
+            console.log(data.paras);
+            console.log(data.decomp_sales);
+            console.log("Optimal total is " + data.total_sales);
+        } else {
+            console.log("Optimization failed");
+        }
+    }
+});
